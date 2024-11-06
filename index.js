671fdc012ff5318429fdce3b459ec7f1d6f9d883
james chambers <jamcha123@localhost.localdomain>
2024-11-06 19:56:24 +0100

    fixed the web scraper and the socket.io part but socket.io emits events twice

--- index.js
+++ index.js
@@ -24,8 +24,18 @@ const server = http.createServer(app)
 const io = new Server(server)
 io.on("connection", (socket) => {
     socket.on("price", (msg) => {
-        const link = "https://api.metalpriceapi.com/v1/latest?api_key=0d9c48bd888d9b9baf10b0a59a2e083f&base=${msg[1]}&currencies=${msg[2]}";
-        window.location.href = link
+        const link = "https://api.metalpriceapi.com/v1/latest?api_key=0d9c48bd888d9b9baf10b0a59a2e083f&base=" + msg[1] + "&currencies=" + msg[2] + ""
+        let target = ""
+        axios.get(link, {
+            method: "get",
+            url: "/v1/latest?api_key=0d9c48bd888d9b9baf10b0a59a2e083f&base=" + msg[1] + "&currencies=" + msg[2] + "",
+            responseType: "json",
+        }).then((value) => {
+            target = value["data"]["rates"]["" + msg[2] + ""] * msg[0];
+            console.log(target)
+            socket.emit("back", target)
+        })
+        target = ""
     })
 })
 server.listen(5173, () => {console.log("http://127.0.0.1:5173/index.html")});
